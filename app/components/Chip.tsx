2b637cb00d659598abde5225d77ced5790669162
ryanadhi <ryanadhi.p@gmail.com>
2025-12-07 11:45:24 +0800

    chore: Upgrade Next.js, React, and TailwindCSS to their latest versions, updating configurations and minor UI elements.

--- app/components/Chip.tsx
+++ app/components/Chip.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Chip = (props: { text: string }) => {
   const { text } = props;
   return (
-    <div className="border border-slate-200 rounded-md bg-slate-200 hover:bg-slate-100 text-slate-800 hover:text-slate-800 px-1 md:py-1 md:px-2 hover:border-slate-800" role="button">
+    <div className="border border-slate-200 rounded-md bg-slate-200 hover:bg-slate-100 text-slate-800 hover:text-slate-800 px-1 md:py-1 md:px-2 hover:border-slate-800 cursor-pointer" role="button">
       <span className="text-xs md:text-sm ">{text}</span>
     </div>
   );
